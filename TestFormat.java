b259aac5845a99d6c6fa82b10709aabec3cf9f91
Mike Hewitt <matroxmike@icloud.com>
2021-04-16 00:50:18 +1000

    more locale number formatting
    more locale number formatting

--- TestFormat.java
+++ TestFormat.java
@@ -1,17 +1,21 @@
 import java.util.Calendar;
 import java.util.Locale;
+import java.text.NumberFormat;
 
 public class TestFormat {
-    
+
     public static void main(String[] args) {
+		NumberFormat nf = NumberFormat.getInstance();
       long n = 461012;
+      double dd = 123456.123;
+      System.out.println(nf.format(dd) + "; ");
       System.out.format("%d%n", ++n);      //  -->  "461012"
       System.out.format("%08d%n", ++n);    //  -->  "00461012"
       System.out.format("%+8d%n", ++n);    //  -->  " +461012"
       System.out.format("%,8d%n", ++n);    // -->  " 461,012"
       System.out.format("%+,8d%n%n", ++n); //  -->  "+461,012"
       System.out.format(Locale.FRANCE,"%+,8d%n%n", ++n); //xx  -->  "+461,012"
-      
+
       double pi = Math.PI;
 
       System.out.format("%f%n", pi);       // -->  "3.141593"
@@ -27,12 +31,15 @@ public class TestFormat {
       System.out.format("%tl:%tM %tp%n", c, c, c);  // -->  "2:34 am"
 
       System.out.format("%tD%n", c);    // -->  "05/29/06"
-      double d = 996.123;
+      double d = 2996.123;
        for (int x = 996 ; x < 1004 ; x++) {
+		//   NumberFormat
+		   nf = NumberFormat.getInstance(Locale.UK );
+		   System.out.format(nf.format(d) + "xx");
             System.out.format(Locale.FRANCE,"%+,8d %+,8d%n", x, -x); //xx  -->  "+461,012"
             System.out.format(Locale.UK,"%+,8d %+,8d%n", x, -x); //xx  -->  "+461,012"
-            System.out.format(Locale.US,"%+,8d %+,8d%n", x, -x); //xx  -->  "+461,012"       
-            System.out.format(Locale.GERMANY,"%+,8d %+,8d%n", x, -x); //xx  -->  "+461,012"        
+            System.out.format(Locale.US,"%+,8d %+,8d%n", x, -x); //xx  -->  "+461,012"
+            System.out.format(Locale.GERMANY,"%+,8d %+,8d%n", x, -x); //xx  -->  "+461,012"
 			}
     }
 }
