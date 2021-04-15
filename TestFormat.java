6d80a6e0536f88fb667dcba731375b455b815957
Mike on Tecra <matroxmike@icloud.com>
2021-04-15 16:39:08 +1000

    different locales

--- TestFormat.java
+++ TestFormat.java
@@ -5,20 +5,21 @@ public class TestFormat {
     
     public static void main(String[] args) {
       long n = 461012;
-      System.out.format("%d%n", n);      //  -->  "461012"
-      System.out.format("%08d%n", n);    //  -->  "00461012"
-      System.out.format("%+8d%n", n);    //  -->  " +461012"
-      System.out.format("%,8d%n", n);    // -->  " 461,012"
-      System.out.format("%+,8d%n%n", n); //  -->  "+461,012"
+      System.out.format("%d%n", ++n);      //  -->  "461012"
+      System.out.format("%08d%n", ++n);    //  -->  "00461012"
+      System.out.format("%+8d%n", ++n);    //  -->  " +461012"
+      System.out.format("%,8d%n", ++n);    // -->  " 461,012"
+      System.out.format("%+,8d%n%n", ++n); //  -->  "+461,012"
+      System.out.format(Locale.FRANCE,"%+,8d%n%n", ++n); //xx  -->  "+461,012"
       
       double pi = Math.PI;
 
       System.out.format("%f%n", pi);       // -->  "3.141593"
-      System.out.format("%.3f%n", pi);     // -->  "3.142"
-      System.out.format("%10.3f%n", pi);   // -->  "     3.142"
-      System.out.format("%-10.3f%n", pi);  // -->  "3.142"
+      System.out.format("%.3f%n", ++pi);     // -->  "3.142"
+      System.out.format("%10.3f%n", ++pi);   // -->  "     3.142"
+      System.out.format("%-10.3f%n", ++pi);  // -->  "3.142"
       System.out.format(Locale.FRANCE,
-                        "%-10.4f%n%n", pi); // -->  "3,1416"
+                        "%-10.4f%n%n", ++pi); // -->  "3,1416"
 
       Calendar c = Calendar.getInstance();
       System.out.format("%tB %te, %tY%n", c, c, c); // -->  "May 29, 2006"
@@ -26,5 +27,12 @@ public class TestFormat {
       System.out.format("%tl:%tM %tp%n", c, c, c);  // -->  "2:34 am"
 
       System.out.format("%tD%n", c);    // -->  "05/29/06"
+      double d = 996.123;
+       for (int x = 996 ; x < 1004 ; x++) {
+            System.out.format(Locale.FRANCE,"%+,8d %+,8d%n", x, -x); //xx  -->  "+461,012"
+            System.out.format(Locale.UK,"%+,8d %+,8d%n", x, -x); //xx  -->  "+461,012"
+            System.out.format(Locale.US,"%+,8d %+,8d%n", x, -x); //xx  -->  "+461,012"       
+            System.out.format(Locale.GERMANY,"%+,8d %+,8d%n", x, -x); //xx  -->  "+461,012"        
+			}
     }
 }
